17421796985db0394d63b718b6186cef01cd2184
Bruno Krebs <bruno.krebs@auth0.com>
2018-02-28 15:11:52 -0300

    adding Author entity;

---
+++ src/Repository/AuthorRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Author;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Author|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Author|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Author[]    findAll()
+ * @method Author[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AuthorRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Author::class);
+    }
+
+    /*
+    public function findBySomething($value)
+    {
+        return $this->createQueryBuilder('a')
+            ->where('a.something = :value')->setParameter('value', $value)
+            ->orderBy('a.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+}
